fcc6f704df9255abd4af890a20fd7203e17a2c3b
MerveKaragoz1 <mervekaragoz13@gmail.com>
2021-01-20 17:32:06 +0300

    classlar eklendi.

---
+++ MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri eklendi. " + musteri.Ad + musteri.Soyad + ".");
+        }
+       
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad);
+            Console.WriteLine(musteri.Soyad);
+            Console.WriteLine(musteri.Id + "\n");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Merve";
+            musteri1.Soyad = "Karagöz";
+            musteri1.Id = 111;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Büşra";
+            musteri2.Soyad = "Karagöz";
+            musteri2.Id = 222;
+
+            //Musteri[] musteriler = new Musteri[] {musteri1, musteri2};
+
+            /*foreach (Musteri musteri in musteriler)
+            {
+                 Console.WriteLine(musteri.Ad);
+                 Console.WriteLine(musteri.Soyad);
+                 Console.WriteLine(musteri.Id);
+                 Console.WriteLine("\n");
+             }*/
+
+                MusteriManager musteriManagerAdd = new MusteriManager();
+                musteriManagerAdd.Ekle(musteri1);
+                musteriManagerAdd.Ekle(musteri2);
+                Console.WriteLine("\n");
+                MusteriManager musteriManagerList = new MusteriManager();
+                musteriManagerList.Listele(musteri1);
+                musteriManagerList.Listele(musteri2);
+
+                /* MusteriManager musteriManagerRemove = new MusteriManager();
+                 musteriManagerRemove.Sil(musteri1);
+                 musteriManagerRemove.Sil(musteri12);*/
+            
         }
     }
 }
